5dacc60ae1a21d7b2ea04b60aba2b5f983a970af
Rover CS Team <stanfordroboclub@gmail.com>
2018-02-24 15:54:56 -0800

    Modified to get segmentation to work with rover set-up

--- output_view/src/output_view.cpp
+++ output_view/src/output_view.cpp
@@ -22,9 +22,9 @@ class ImageConverter
   ros::Publisher image_pub;
   double alpha;
   cv::Mat lookUpTable;
-  
+
 public:
-  
+
   ImageConverter(const std::string &colors_file, double a, const std::string &image_output)
   {
     /*
@@ -32,7 +32,7 @@ public:
     image_pub = nh.advertise<sensor_msgs::Image>(image_output, 1);
     */
     alpha = a;
-    
+
     // TODO - undefined behavior when getting in more colors than it expects
     lookUpTable = cv::Mat(1, 256, CV_8UC3);
 
@@ -43,16 +43,17 @@ public:
 
   ~ImageConverter()
   {
-    cv::destroyWindow(OPENCV_WINDOW);
+      cv::destroyWindow(OPENCV_WINDOW);
   }
-  
+
   void segmentationCb(const ImageConstPtr& raw_image, const ImageConstPtr& seg_info)
   {
     cv_bridge::CvImageConstPtr raw_image_ptr;
     cv_bridge::CvImageConstPtr seg_info_ptr;
     try
     {
-      raw_image_ptr = cv_bridge::toCvShare(raw_image, sensor_msgs::image_encodings::BGR8);
+      //      raw_image_ptr = cv_bridge::toCvShare(raw_image, sensor_msgs::image_encodings::BGR8);
+      raw_image_ptr = cv_bridge::toCvShare(raw_image, raw_image->encoding);
       seg_info_ptr = cv_bridge::toCvShare(seg_info, sensor_msgs::image_encodings::MONO8);
     }
     catch (cv_bridge::Exception& e)
@@ -60,7 +61,7 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-    
+
     cv::Mat colored_image(seg_info_ptr->image.size(), CV_8UC3);
     colorImage(lookUpTable, seg_info_ptr->image, colored_image);
 
@@ -70,9 +71,10 @@ public:
 
     cv::Mat final_image;
     cv::addWeighted(scaled_image, alpha, raw_image_ptr->image, 1 - alpha, 0.0, final_image);
-    
+
     // Update GUI Window
-    cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_NORMAL);
+    //cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_NORMAL);
+    cv::namedWindow(OPENCV_WINDOW, CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_EXPANDED);
     cv::imshow(OPENCV_WINDOW, final_image);
     cv::waitKey(3);
 
@@ -93,7 +95,7 @@ int main(int argc, char** argv)
   std::string annotation_input, annotation_type;
   std::string colors_file;
   double alpha;
-  
+
   nh.param<std::string>("image_input", image_input, "/camera/image_raw");
   nh.param<std::string>("annotation_input", annotation_input, "/segnet/network/output");
   nh.param<std::string>("annotation_type", annotation_type, "segmentation");
@@ -102,15 +104,15 @@ int main(int argc, char** argv)
   nh.param("alpha", alpha, 0.8);
 
   ImageConverter img(colors_file, alpha, image_output);
-  
+
   // TODO - time sync code cannot be put into the constructor of ImageView...why?
   // TODO - this needs to be a class only because of cv::destroyWindow
   ROS_INFO("Now subscribing to raw image topic: %s", image_input.c_str());
   message_filters::Subscriber<Image> image_sub(nh, image_input, 1);
-    
+
   ROS_INFO("Now subscribing to data topic: %s (%s)", annotation_input.c_str(), annotation_type.c_str());
   message_filters::Subscriber<Image> info_sub(nh, annotation_input, 1);
-  
+
   TimeSynchronizer<Image, Image> sync(image_sub, info_sub, 10);
 
   if (annotation_type == "segmentation") {
@@ -118,7 +120,7 @@ int main(int argc, char** argv)
   } else {
     ROS_ERROR("Annotation type not supported");
   }
-  
+
   ros::spin();
   return 0;
 }
